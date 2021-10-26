d071577ef0a7d20c8302805e6b7a3a6ddfff4810
宗子竣 <luckyjackie30@sina.com>
2021-10-26 15:33:30 -0400

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,22 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "semi": ["warn", "never"],
+    "quotes": ["warn", "single"],
+    "key-spacing": ["warn", { "beforeColon": false, "afterColon": true }],
+    "new-cap": ["warn", { "newIsCap": true, "capIsNew": false}],
+    "no-multiple-empty-lines": ["warn", {"max": 1}],
+    "comma-style": ["warn", "last"],
+    "comma-spacing": ["warn", { "before": false, "after": true }],
+    "comma-dangle": ["warn", "always-multiline"],
+    "no-implicit-coercion": "warn",
+    "no-invalid-this": "warn",
+    "no-multi-spaces": "warn",
+    "no-new-func": "warn",
+    "global-require": "warn",
+    // 'no-console': 'warn',
+    'object-curly-spacing': ['warn', 'always'],
+    "space-before-function-paren": ['warn', "always"],
   }
 }
